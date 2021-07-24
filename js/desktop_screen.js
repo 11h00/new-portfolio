6faf798043353b03d8b773f5351f94dfc86b5a1b
11h00 <tpjdog@gmail.com>
2021-07-24 11:21:56 +0200

    added inactive tag and now can load desktop

--- js/desktop_screen.js
+++ js/desktop_screen.js
@@ -1,3 +1,6 @@
 let load_guest_desktop = () => {
-    
+    document.querySelectorAll("section").forEach(ww => ww.setAttribute("inactive", "true"))
+    setTimeout(() => {
+        document.querySelector("#logging_in_screen").removeAttribute("inactive")
+    })
 }
