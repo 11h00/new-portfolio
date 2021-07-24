7f471d18761728fe5b7f526fa1ed2cf95504be66
11h00 <tpjdog@gmail.com>
2021-07-24 12:27:05 +0200

    added constructor for handle width and height

---
+++ js/window.js
@@ -0,0 +1,12 @@
+class windows{
+    width = 400
+    height = 200
+    constructor(width = this.width, height = this.height){
+        this.width = width;
+        this.height = height
+        this.open_window(width, height)
+    }
+    open_window(width, height){
+
+    }
+}
