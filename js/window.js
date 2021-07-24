aefa11943abcfa33916c78830e87229d4e8206e8
11h00 <tpjdog@gmail.com>
2021-07-24 12:50:47 +0200

    added a way to do a unique window per instatation and incresed interval of double click

--- js/window.js
+++ js/window.js
@@ -1,15 +1,19 @@
 class windows{
     width = 400
     height = 200
+    id;
     constructor(width = this.width, height = this.height){
         this.width = width;
         this.height = height
+        this.id = "window_" + new Date().getTime();
         this.open_window(width, height)
     }
-    open_window(width, height){
-
+    open_window(width, height, title, text){
+        document.querySelector("section.desktop").insertAdjacentHTML("afterbegin", `
+            <window style="position: absolute;width: ${this.width};height: ${this.height};"></window>
+        `);
     }
     close_window(){
-        
+
     }
 }
