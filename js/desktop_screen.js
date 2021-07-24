7dc760a01057dd4ea2cd8f2aa2e1c4a92a453642
11h00 <tpjdog@gmail.com>
2021-07-24 12:23:02 +0200

    added a double clikc detection for files

--- js/desktop_screen.js
+++ js/desktop_screen.js
@@ -1,6 +1,12 @@
+let double_clicks = 0;
 let load_guest_desktop = () => {
     document.querySelectorAll("section").forEach(ww => ww.setAttribute("inactive", "true"))
     setTimeout(() => {
         document.querySelector("#guest_desktop_screen").removeAttribute("inactive")
     }, 3000)
 }
+setInterval(() => {
+    if (double_clicks != 0){
+        double_clicks = 0
+    }
+}, 400);
