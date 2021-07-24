09f3901be9aed8370d04c38010fe25349b7b9504
11h00 <tpjdog@gmail.com>
2021-07-24 15:03:01 +0200

    added mathias desktop loading

--- js/desktop_screen.js
+++ js/desktop_screen.js
@@ -22,6 +22,30 @@ let load_guest_desktop = () => {
         }, 2000)
     })
 }
+
+let load_mathias_desktop = () => {
+    document.querySelectorAll("section").forEach(ww => ww.setAttribute("inactive", "true"))
+    setTimeout(() => {
+        document.querySelector("#mathias777_desktop_screen").removeAttribute("inactive")
+    }, 3000)
+    document.querySelectorAll(".file").forEach(el => {
+        el.addEventListener("click", (evn) => {
+            if (double_clicks){
+                console.log("action")
+                console.log(evn.currentTarget.children[1].innerText)
+                let titll = evn.currentTarget.children[1].innerText
+                let conren = evn.currentTarget.children[1].attributes.cdn.value
+                new windows(titll, conren)
+            }
+        })
+    })
+    document.getElementById("log_me_out").addEventListener("click", () => {
+        document.querySelectorAll("section").forEach(ww => ww.setAttribute("inactive", "true"))
+        setTimeout(() => {
+            document.querySelector("#login_screen").removeAttribute("inactive")
+        }, 2000)
+    })
+}
 setInterval(() => {
     if (double_clicks != 0){
         double_clicks = 0
