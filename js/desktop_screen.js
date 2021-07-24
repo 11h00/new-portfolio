f68862c9bfa20966579061d712af1c07954ea359
11h00 <tpjdog@gmail.com>
2021-07-24 11:24:21 +0200

    calling wrong screen and was not waiting

--- js/desktop_screen.js
+++ js/desktop_screen.js
@@ -1,6 +1,6 @@
 let load_guest_desktop = () => {
     document.querySelectorAll("section").forEach(ww => ww.setAttribute("inactive", "true"))
     setTimeout(() => {
-        document.querySelector("#logging_in_screen").removeAttribute("inactive")
+        document.querySelector("#guest_desktop_screen").removeAttribute("inactive")
     }, 3000)
 }
